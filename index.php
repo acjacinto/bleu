4836331b015dbddc9df3fa339af6437b8c56723b
AC Jacinto <36821798+acjacinto@users.noreply.github.com>
2023-02-23 15:59:59 +0800

    up

--- index.php
+++ index.php
@@ -713,7 +713,7 @@
           <div class="col-lg-6">
             <div class="map mb-4 mb-lg-0">
               <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 312px;" allowfullscreen></iframe> -->
-                <iframe src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJFSzZytnJlzMR_i3_HwAKPx4&key=AIzaSyDDXbplbVUU4OAra24M8bs1WUi2iEW2ajc" frameborder="0" style="border:0; width: 100%; height: 350px;" allowfullscreen></iframe>
+                <iframe src="#" frameborder="0" style="border:0; width: 100%; height: 350px;" allowfullscreen></iframe>
             </div>
           </div>
 
